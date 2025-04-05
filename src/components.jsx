e2a6bc32b3a0b45e382dc61f924a44437fb5837c
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-04-05 05:39:53 +0000

    rename lab title

--- src/components.jsx
+++ src/components.jsx
@@ -1,5 +1,7 @@
 import {hello} from './functions.js';
 
+<h1>Lab 7 Questions</h1>
+
 function Question1(){
     return <section>
 1. Write a function that takes a _name_ as an argument and _returns a string_ that _contains "hello"_ and the name paramerer. Test at least 3 names. <br />*Note ... I solved this one already
