ea75e3041094de478640f0e9cb9a8ca35727972c
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-04-05 06:19:22 +0000

    reformat questions

--- src/components.jsx
+++ src/components.jsx
@@ -4,8 +4,8 @@ function Question0(){
     return <section>
     <h1>Lab 7 Questions</h1>
 
-0. Write a function that converts fahrenheit to celsius.
-    <h2>Results</h2>   
+<h2>0. Write a function that converts fahrenheit to celsius</h2>
+    <h3>Results</h3>   
     <p>FtoC(32) == "{FtoC(32)}"</p>     
     <p>FtoC(212) == "{FtoC(212)}"</p> 
     <p>FtoC(70) == "{FtoC(70)}"</p>
@@ -14,8 +14,8 @@ function Question0(){
 
 function Question1(){
     return <section>
-1. Write a function that takes a _name_ as an argument and _returns a string_ that _contains "hello"_ and the name paramerer. Test at least 3 names. <br />*Note ... I solved this one already
-      <h2>results</h2>
+<h2>1. Write a function that takes a _name_ as an argument and _returns a string_ that _contains "hello"_ and the name paramerer. Test at least 3 names. <br />*Note ... I solved this one already</h2>
+      <h3>Results</h3>
       <p>hello("Rich") == "{hello('Rich')}"</p>
       <p>hello("Bill") == "{hello('Bill')}"</p>
       <p>hello("Chris") == "{hello('Chris')}"</p>
@@ -24,8 +24,8 @@ function Question1(){
 
 function Question2(){
     return <section>
-2. There are 43,560 square feet per acre. Write a program that converts square feet to acres. 
-    <h2>Results</h2>
+<h2>2. There are 43,560 square feet per acre. Write a program that converts square feet to acres</h2>
+    <h3>Results</h3>
     <p>SqftToAcres(250000) == "{SqftToAcres(250000)}"</p>
     <p>SqftToAcres(925000) == "{SqftToAcres(925000)}"</p>
     <p>SqftToAcres(2000000) == "{SqftToAcres(2000000)}"</p>
@@ -34,8 +34,8 @@ function Question2(){
 
 function Question3(){
     return <section>
-3. Given the width of a lawn in metres, length of a lawn in metres and the square metres cut per minute calculate the minutes it would take to mow that lawn.
-    <h2>Results</h2>
+<h2>3. Given the width of a lawn in metres, length of a lawn in metres and the square metres cut per minute calculate the minutes it would take to mow that lawn.</h2>
+<h3>Results</h3>
     <p>TimeToMow(2, 5, 1) == "{TimeToMow(2, 5, 1)}"</p>
     <p>TimeToMow(3, 4, 2) == "{TimeToMow(3, 4, 2)}"</p>
     <p>TimeToMow(10, 8, 2) == "{TimeToMow(10, 8, 2)}"</p>        
@@ -45,6 +45,13 @@ function Question3(){
 function Question4(){
     return <section>
     <h2>4. Compute the air quality given an air quality index.</h2>
+    <li> Good: 0 to 50 AQI</li>
+    <li> Moderate: 51 - 100 AQI</li>
+    <li> Unhealthy for Sensitive Groups: 101 - 150 AQI</li>
+    <li> Unhealthy: 151 - 200 AQI</li>
+    <li> Very Unhealthy: 201 - 300 AQI</li>
+    <li> Hazardous: 300+ AQI</li>
+    
     <h3>Results</h3>
     <p>computeAQI(4) == "{computeAQI(4)}"</p>
     <p>computeAQI(62) == "{computeAQI(62)}"</p>
